bcebfa42da0d762d8bc1e06f40353209dc45621c
eyeck3 <43934315+eyeck3@users.noreply.github.com>
2020-03-04 15:20:13 -0600

    Milestone 2 Step 3

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.gms.location.LocationServices;
@@ -13,6 +18,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class MainActivity extends FragmentActivity {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     private FusedLocationProviderClient mFusedLocationProviderClient; // Save the instance
     // Bascom Hall
     private final LatLng mDestinationLatLng = new LatLng(43.0753796,-89.4064015);
@@ -36,9 +42,40 @@ public class MainActivity extends FragmentActivity {
                     .title("Destination"));
 
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLng, 17f));
+
+                displayMyLocation();
         });
 
         // Obtain a FusedLocationProviderClient
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation() {
+        // Check if permission is granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        // If not, ask for it
+        if (permission== PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // If permission granted, display marker at current location
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
     }
 }
