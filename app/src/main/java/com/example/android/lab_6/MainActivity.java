09b79138afcccec548db983220a2ef19abd45158
eyeck3 <43934315+eyeck3@users.noreply.github.com>
2020-03-04 15:04:56 -0600

    Milestone 2 Step 1

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.android.lab_6;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class MainActivity extends FragmentActivity {
 
-    // Somewhere in Australia
+    private FusedLocationProviderClient mFusedLocationProviderClient; // Save the instance
+    // Bascom Hall
     private final LatLng mDestinationLatLng = new LatLng(43.0753796,-89.4064015);
     private GoogleMap mMap;
 
@@ -34,5 +37,8 @@ public class MainActivity extends FragmentActivity {
 
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLng, 17f));
         });
+
+        // Obtain a FusedLocationProviderClient
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 }
